ff02584a15b166e543c07e14e62e2c4da7b07dea
emericbarbot-lab <emeric.barbot@devoteam.com>
2026-09-21 14:17:57 +0200

    Remove redundant requests import and API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 import os
 from snowflake.snowpark.functions import col
+import requests
+
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw: {st.__version__}")
@@ -33,8 +35,7 @@ ingredients_list=st.multiselect(
         
 if ingredients_list:
     ingredients_string=''
-      for fruit_chosen in ingredients_list:
-      import requests  
+    for fruit_chosen in ingredients_list:
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
       #st.text(smoothiefroot_response.json)
       sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
@@ -58,7 +59,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-    import requests  
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-    #st.text(smoothiefroot_response.json)
-    sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
