c82d7b4f4fdf33a3caff633a1ce1e7eecbb8142b
emericbarbot-lab <emeric.barbot@devoteam.com>
2026-09-21 11:28:24 +0200

    Implement order submission and fruit API call
    Add functionality to submit smoothie orders and fetch fruit data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,8 @@ if ingredients_list:
 
     my_insert_stmt = f"""insert into smoothies.public.orders(ingredients, name_on_order)
             values ('{ingredients_string}', '{name_of_order}')"""
+
+
     
 
     time_to_insert=st.button('summit order')
@@ -46,3 +48,8 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+
+    import requests  
+    smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+    st.text(smoothiefroot_response)
